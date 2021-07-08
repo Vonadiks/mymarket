b304acbe4c849eb36e4b5cd587e854bbbd148614
Sergey Skidanov <vonadiks@mail.ru>
2021-07-08 15:16:35 +0400

    initial commit

---
+++ src/test/java/ru/geekbrains/my/market/MyMarketApplicationTests.java
@@ -0,0 +1,13 @@
+package ru.geekbrains.my.market;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MyMarketApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
