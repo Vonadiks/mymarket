bea3991d0eddb87f32541cb26e68f5f5fc4c2e65
Sergey Skidanov <vonadiks@mail.ru>
2021-07-19 01:31:38 +0400

    spring1 lesson7

--- src/main/java/ru/geekbrains/my/market/controllers/ProductController.java
+++ src/main/java/ru/geekbrains/my/market/controllers/ProductController.java
@@ -1,49 +1,114 @@
 package ru.geekbrains.my.market.controllers;
 
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.stereotype.Controller;
-import org.springframework.ui.Model;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestParam;
+import lombok.RequiredArgsConstructor;
+import org.springframework.web.bind.annotation.*;
+import ru.geekbrains.my.market.model.Product;
 import ru.geekbrains.my.market.services.ProductService;
 
-import java.math.BigDecimal;
+import java.util.List;
 
-@Controller
+@RestController
+@RequiredArgsConstructor
 public class ProductController {
-    ProductService productService;
+    private final ProductService productService;
 
-    @Autowired
-    public ProductController(ProductService productService) {
-        this.productService = productService;
+    //получение всех товаров [ GET .../app/products ]
+    //http://localhost:8080/mymarket/product/all
+    @GetMapping("/product/all")
+    @ResponseBody
+    public List<Product> findAllProducts() {
+        return productService.findAll();
     }
 
-    @GetMapping
-    public String showMainPage(Model model) {
-        model.addAttribute("products", productService.findAll());
-        return "index";
+    //получение товара по id [ GET .../app/products/{id} ]
+    //http://localhost:8080/mymarket/product/{id}
+
+    @GetMapping("/product/{id}")
+    @ResponseBody
+    public Product findProductById(@PathVariable Long id) {
+        return productService.findById(id);
     }
+    //удаление товара по id.[ GET .../app/products/delete/{id} ]
+    //http://localhost:8080/mymarket/product/delete/{id}
+
+    @GetMapping("/product/delete/{id}")
+    @ResponseBody
+    public List<Product> deleteProductById(@PathVariable Long id) {
+        productService.deleteById(id);
+        return productService.findAll();
+    }
+
+    //создание нового товара [ POST .../app/products ]
+    //http://localhost:8080/mymarket/product/add?title=prod1&price=10
 
     @GetMapping("/product/add")
-    public String showAddProductForm() {
-        return "add_product_form";
+    @ResponseBody
+    public List<Product> addNewProduct(@RequestParam String title, @RequestParam int price) {
+        saveNewProduct(title, price);
+        return productService.findAll();
     }
 
     @PostMapping("/product/add")
-    public String addProduct(@RequestParam String title, @RequestParam BigDecimal price) {
-        productService.addProduct(title, price);
-        return "redirect:/";
+    public String saveNewProduct(@RequestParam String title, @RequestParam int price) {
+        productService.addNewProduct(title, price);
+        return "redirect: /";
     }
 
-    @GetMapping("/product/{id}")
-    public String showProductInfo(Model model, @PathVariable Long id) {
-        model.addAttribute("product", productService.findById(id));
-       //model.addAttribute("title", productService.findById(id).getTitle());
-        //model.addAttribute("price", productService.findById(id).getPrice());
-        return "product_info_view";
+    //товары дороже min цены
+    //http://localhost:8080/mymarket/product/more_than_min_price?price=100
+
+    @GetMapping("/product/more_than_min_price")
+    @ResponseBody
+    public List<Product> findMoreThanMinPrice(@RequestParam  int price) {
+        return productService.findByMinPrice(price);
     }
+
+    //товары дешевле максимальной цены
+    //http://localhost:8080/mymarket/product/less_than_max_price?price=100
+
+    @GetMapping("/product/less_than_max_price")
+    @ResponseBody
+    public List<Product> findLessThanMaxPrice(@RequestParam  int price) {
+        return productService.findByMaxPrice(price);
+    }
+
+
+//    @GetMapping
+//    public String showMainPage(Model model) {
+//        model.addAttribute("products", productService.findAll());
+//        return "index";
+//    }
+//
+//    @GetMapping("/product/add")
+//    public String showAddProductForm() {
+//        return "add_product_form";
+//    }
+//
+//    @PostMapping("/product/add")
+//    public String addProduct(@RequestParam String title, @RequestParam int price) {
+//        productService.addProduct(title, price);
+//        return "redirect:/";
+//    }
+//
+//    @GetMapping("/product/{id}")
+//    public String showProductInfo(Model model, @PathVariable Long id) {
+//        model.addAttribute("product", productService.findById(id));
+//       //model.addAttribute("title", productService.findById(id).getTitle());
+//        //model.addAttribute("price", productService.findById(id).getPrice());
+//        return "product_info_view";
+//    }
+//
+//    @GetMapping("/product/find_by_price")
+//    @ResponseBody
+//    public List<Product> findByMinPrice(@RequestParam(name = "min") int min) {
+//            return productService.findByMinPrice(min);
+//    }
+//
+//    @GetMapping("/product/find_by_id_max")
+//    @ResponseBody
+//    public List<Product> findByMaxId(@RequestParam(name = "max") Long max) {
+//        return productService.findByIdLessMax(max);
+//    }
 
 
 }
--- src/main/java/ru/geekbrains/my/market/model/Product.java
+++ src/main/java/ru/geekbrains/my/market/model/Product.java
@@ -1,42 +1,24 @@
 package ru.geekbrains.my.market.model;
 
-import java.math.BigDecimal;
 
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import javax.persistence.*;
+
+
+@Entity
+@Table (name = "products")
+@Data
+@NoArgsConstructor
 public class Product {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "id")
     private Long id;
+    @Column(name = "title")
     private String title;
-    private BigDecimal price;
+    @Column(name = "price")
+    private int price;
 
-    public Product() {
-    }
-
-    public Product(Long id, String title, BigDecimal price) {
-        this.id = id;
-        this.title = title;
-        this.price = price;
-    }
-
-    public Long getId() {
-        return id;
-    }
-
-    public void setId(Long id) {
-        this.id = id;
-    }
-
-    public String getTitle() {
-        return title;
-    }
-
-    public void setTitle(String title) {
-        this.title = title;
-    }
-
-    public BigDecimal getPrice() {
-        return price;
-    }
-
-    public void setPrice(BigDecimal price) {
-        this.price = price;
-    }
 }
