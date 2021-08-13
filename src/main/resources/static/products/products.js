ce86bcb7c8ca23a0a9c70ffdd73a9e19d1face4c
Sergey Skidanov <vonadiks@mail.ru>
2021-08-13 13:04:27 +0400

    some changes1

---
+++ src/main/resources/static/products/products.js
@@ -0,0 +1,34 @@
+angular.module('app').controller('productsController', function ($scope, $http, $localStorage) {
+    const contextPath = 'http://localhost:8080/mymarket/';
+
+    $scope.loadPage = function(pageIndex = 1) {
+                $http({
+                    url: contextPath + '/api/v1/products',
+                    method: 'GET',
+                    params: {
+                    'p': pageIndex
+                    }
+                }).then(function (response) {
+                    $scope.productsPage = response.data;
+                    $scope.navList = $scope.generatePagesIndexes(1, $scope.productsPage.totalPages);
+                     console.log(response);
+                });
+            };
+    $scope.addToCart = function(productId) {
+                         $http({
+                                url: contextPath + '/api/v1/cart/add/' + productId,
+                                method: 'GET',
+                                params: {}
+                         }).then(function (response) {
+                                  $scope.loadCart();
+                         });
+                     };
+    $scope.generatePagesIndexes = function (startPage, endPage) {
+                    let arr = [];
+                    for (let i = startPage; i < endPage + 1; i++) {
+                        arr.push(i);
+                    }
+                    return arr;
+                }
+    $scope.loadPage();
+});
